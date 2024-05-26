dcc638d8079659a63ca51eb4dd78ffe5d041caf2
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-26 13:27:51 -0300

    Add files, screen about

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Terminal from '../../Components/Terminal';
 import About from '../About';
-import { Container, Introduction, SecondPages } from './styled';
+import { Container, Introduction } from './styled';
 
 const Home = () => {
     return (
@@ -10,6 +10,7 @@ const Home = () => {
                     <h1>Desenvolvedor FullStack</h1>
                     <br />
                     <br />
+                    <p>Principais redes, linkedin, gmail e github</p>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque suscipit pariatur totam laborum autem quos fugiat, quas magni impedit ab doloribus temporibus enim
                         molestias reiciendis facilis laudantium quo molestiae!
