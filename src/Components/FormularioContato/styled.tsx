22d5ed2d10a3c01a1a2c48a702b1080c2ef9b381
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-28 20:40:26 -0300

    FormsContato

--- src/Components/FormularioContato/styled.tsx
+++ src/Components/FormularioContato/styled.tsx
@@ -3,10 +3,73 @@ import styled from 'styled-components';
 export const FormMainContact = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    gap: 2rem;
+    border: 1px solid black;
 `;
 export const FormContact = styled.form`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    max-width: 700px;
+`;
+
+export const NameAndEmailInput = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 2rem;
+    margin-bottom: 1rem;
+`;
+
+export const NameInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0px 0px 0px 0px;
+    input {
+        padding: 10px;
+    }
+`;
+export const EmailInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0px 0px 0px 0px;
+    input {
+        padding: 10px;
+    }
+`;
+
+export const MensageAndButton = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
+export const MensageInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    input {
+        width: 300px;
+        height: 300px;
+    }
+    textarea {
+        width: 600px;
+        height: 300px;
+        padding: 10px;
+        font-size: 16px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        resize: none; /* Impede a alteração do tamanho */
+    }
+`;
+export const ButtonInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    button {
+        padding: 10px;
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
 `;
