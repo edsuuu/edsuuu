e809473704eca31b503528dea1a3c536e1756c12
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-28 19:49:09 -0300

    Add Components RedesSociais

---
+++ src/Components/RedesSociais/index.tsx
@@ -0,0 +1,10 @@
+export default function RedesSociais() {
+    return (
+        <div>
+            <p>Linkedin</p>
+            <p>Github</p>
+            <p>Gmail</p>
+            <p>Gmail</p>
+        </div>
+    );
+}
