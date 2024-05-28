585553ef2b65b212722e0979255a2d69df164fb9
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-28 20:40:26 -0300

    FormsContato

--- src/Components/FormularioContato/index.tsx
+++ src/Components/FormularioContato/index.tsx
@@ -1,41 +1,35 @@
-import { FormContact, FormMainContact } from "./styled";
+import { FormContact, NameAndEmailInput, NameInput, EmailInput, MensageInput, ButtonInput, MensageAndButton } from './styled';
 
 export default function FormularioContato() {
     return (
-        <FormMainContact>
-            <h1>Contato</h1>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-            <FormContact id="form" className="form">
-                <h2>Faça seu cadastro</h2>
-
-                <div className="form-fields">
-                    <label htmlFor="username">Seu usuário</label>
+        <FormContact id="form" className="form">
+            <div>
+                <h1>Contato</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, omnis!</p>
+            </div>
+            <NameAndEmailInput>
+                <NameInput>
+                    <label htmlFor="username">Seu Nome</label>
                     <input type="text" name="username" className="username" id="username" placeholder="Seu usuário" />
                     <span className="error-message">Campo inválido por algum motivo qualquer</span>
-                </div>
-
-                <div className="form-fields">
+                </NameInput>
+                <EmailInput>
                     <label htmlFor="email">Seu e-mail</label>
                     <input type="email" name="email" className="email" id="email" placeholder="Seu e-mail" />
                     <span className="error-message">Campo inválido por algum motivo qualquer</span>
-                </div>
-
-                <div className="form-fields">
-                    <label htmlFor="username">Sua senha</label>
-                    <input type="password" name="password" className="password" id="password" placeholder="Sua senha" />
+                </EmailInput>
+            </NameAndEmailInput>
+            <MensageAndButton>
+                <MensageInput>
+                    <label htmlFor="mensage">Sua Mensagem</label>
+                    {/* <input type="text" name="mensage" className="mensage" id="mensage" placeholder="Sua Mensagem" /> */}
+                    <textarea className="message-input" placeholder="Digite sua mensagem aqui..."></textarea>
                     <span className="error-message">Campo inválido por algum motivo qualquer</span>
-                </div>
-
-                <div className="form-fields">
-                    <label htmlFor="username">Repetir senha</label>
-                    <input type="password" name="password2" className="password2" id="password2" placeholder="Repetir senha" />
-                    <span className="error-message">Campo inválido por algum motivo qualquer</span>
-                </div>
-
-                <div className="form-fields">
+                </MensageInput>
+                <ButtonInput>
                     <button type="submit">Enviar</button>
-                </div>
-            </FormContact>
-        </FormMainContact>
+                </ButtonInput>
+            </MensageAndButton>
+        </FormContact>
     );
 }
