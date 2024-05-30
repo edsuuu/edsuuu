cf95237db64395cd79879b4b8b40ffd24f308d7b
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-30 17:34:54 -0300

    Component Redes

--- src/Components/RedesSociais/styled.tsx
+++ src/Components/RedesSociais/styled.tsx
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const Redes = styled.div``;
+export const Redes = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    ul {
+    }
+
+    li {
+        list-style: none;
+    }
+`;
