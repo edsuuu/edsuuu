15d3fa72b72d6f32e14d91a274a8020139e9ca5e
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-30 21:50:59 -0300

    Pré update responsivo Redes Sociais

--- src/Components/RedesSociais/styled.tsx
+++ src/Components/RedesSociais/styled.tsx
@@ -5,11 +5,22 @@ export const Redes = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-around;
-
+    height: auto;
+    width: 250px;
     ul {
     }
 
     li {
         list-style: none;
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: row;
+        width: 100%;
+
+        a {
+            display: flex;
+            flex-direction: row;
+        }
     }
 `;
