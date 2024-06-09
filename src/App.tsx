34d581f91d9f1170e0ef2c86e3842238029eed2e
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-09 12:24:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyled';
 import AppRoutes from './Router';
 import Navigation from './Components/Navbar';
-import Footer from './Components/Footer';
+// import Footer from './Components/Footer';
 
 export default function App(): JSX.Element {
     return (
