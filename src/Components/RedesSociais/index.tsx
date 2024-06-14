4f4462043109a4f9e597d928896648947f10995b
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-14 14:29:30 -0300

    add

--- src/Components/RedesSociais/index.tsx
+++ src/Components/RedesSociais/index.tsx
@@ -1,11 +1,12 @@
 import { FaLinkedin, FaGithub, FaDiscord  } from 'react-icons/fa';
-import { BiLogoGmail } from 'react-icons/bi';
+// import { BiLogoGmail } from 'react-icons/bi';
+import { TbBrandGmail } from "react-icons/tb";
 import { Redes, LinksRedes } from './styled';
 
 const rotas = [
     { name: 'Linkedin', to: 'https://www.linkedin.com/in/edsonlima343', icon: <FaLinkedin size={30} />, className: 'linkedin' },
     { name: 'Github', to: 'https://github.com/edsuuu', icon: <FaGithub size={30} />, className: 'github' },
-    { name: 'Gmail', to: '/gmail', icon: <BiLogoGmail size={30} />, className: 'gmail' },
+    { name: 'Gmail', to: '/gmail', icon: <TbBrandGmail size={30} />, className: 'gmail' },
     { name: 'Discord', to: '/discord', icon: <FaDiscord  size={30} />, className: 'discord' },
 ];
 
--- src/Pages/Projects/index.tsx
+++ src/Pages/Projects/index.tsx
@@ -1,9 +1,22 @@
-import { Container } from "./styled";
+import { Container } from './styled';
 
 const Project = () => {
     return (
         <Container>
-            <h1>Projetos</h1>
+            <div>
+                <div>
+                    <h1>Projetos concluidos</h1>
+                </div>
+                <div>
+                    <h1>Projetos em andamento</h1>
+                </div>
+                <div>
+                    <h1>pequenos projetos</h1>
+                </div>
+                <div>
+                    <h1>mais Repositorios no github</h1>
+                </div>
+            </div>
         </Container>
     );
 };
