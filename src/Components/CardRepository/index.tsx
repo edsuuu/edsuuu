474f8ab81bad176cf6c425f9db604f4d0a9eba26
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-15 18:19:06 -0300

    Bug fix

--- src/Components/CardRepository/index.tsx
+++ src/Components/CardRepository/index.tsx
@@ -4,7 +4,8 @@ import { Container } from './styled';
 interface Post {
     userId: number;
     id: number;
-    name: string;
+    name?: string;
+    title?: string;
     body: string;
 }
 
@@ -15,8 +16,9 @@ const CardRepository: React.FC = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-                const response = await fetch('https://api.github.com/users/edsuuu/repos');
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                // const response = await fetch('https://api.github.com/users/edsuuu/repos');
+                // gerar uma chave para api https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting
                 const data = await response.json();
                 setRepositorios(data);
             } catch (e) {
@@ -43,7 +45,7 @@ const CardRepository: React.FC = () => {
                 {repositorios.slice(0, 10).map((repositorios) => (
                     <div key={repositorios.id} className="repo-card">
                         <img src={`https://via.placeholder.com/150/0000FF/808080?text=Post+${repositorios.id}`} alt="repo" className="repo-image" />
-                        <h2>{repositorios.name}</h2>
+                        <h2>{repositorios.title}</h2>
                     </div>
                 ))}
             </div>
