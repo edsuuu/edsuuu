cb6d89a64ac1186581dd500c6bb43bad6b7602a8
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-15 16:12:02 -0300

    Component MaquinaWrite

---
+++ src/Components/MaquinaWrite/index.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+interface MaquinaWriteProps {
+    text: string;
+    delay?: number;
+    onComplete: () => void;
+}
+
+const MaquinaWrite: React.FC<MaquinaWriteProps> = ({ text, delay = 100, onComplete }) => {
+    const [currentText, setCurrentText] = useState('');
+
+    const writeText = (text: string, i = 0) => {
+        if (i < text.length) {
+            setCurrentText(text.slice(0, i + 1));
+            setTimeout(() => writeText(text, i + 1), delay);
+        } else {
+            setCurrentText(text);
+            onComplete();
+        }
+    };
+
+    useEffect(() => {
+        writeText(text);
+    }, [text]);
+
+    return <h1>{currentText}</h1>;
+};
+
+export default MaquinaWrite;
