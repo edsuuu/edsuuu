5dfab2f8961c3f3c3554fb4f276907c00ea0dff2
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-15 18:12:46 -0300

    Add CardRepo

---
+++ src/Components/CardRepository/index.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from './styled';
+
+interface Post {
+    userId: number;
+    id: number;
+    name: string;
+    body: string;
+}
+
+const CardRepository: React.FC = () => {
+    const [repositorios, setRepositorios] = useState<Post[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const response = await fetch('https://api.github.com/users/edsuuu/repos');
+                const data = await response.json();
+                setRepositorios(data);
+            } catch (e) {
+                console.error('Erro ao buscar os dados:', e);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="loading-screen">
+                <p>Loading...</p>
+            </div>
+        );
+    }
+
+    return (
+        <Container>
+            <div className="card-container">
+                {repositorios.slice(0, 10).map((repositorios) => (
+                    <div key={repositorios.id} className="repo-card">
+                        <img src={`https://via.placeholder.com/150/0000FF/808080?text=Post+${repositorios.id}`} alt="repo" className="repo-image" />
+                        <h2>{repositorios.name}</h2>
+                    </div>
+                ))}
+            </div>
+        </Container>
+    );
+};
+
+export default CardRepository;
