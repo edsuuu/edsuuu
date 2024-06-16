4b499bb39a43ed6caf2480b6a909590b5f0ea400
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-16 19:52:15 -0300

    Fix

--- src/GlobalStyled.tsx
+++ src/GlobalStyled.tsx
@@ -11,7 +11,21 @@ export const GlobalStyle = createGlobalStyle`
 body {
     /* background: #11111a; */
     /* 00a4ef */
-    background-color: #ffffff;
+    /* background-color: #555555; */
+}
+
+.particles-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0; /* Para garantir que está no fundo */
+}
+
+.content-container {
+    position: relative;
+    z-index: 1; /* Para garantir que está acima dos particles */
 }
 
 `;
