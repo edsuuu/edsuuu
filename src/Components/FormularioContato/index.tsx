25083ad28ceef6eef804b3019422bf1395a7d330
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-26 00:35:22 -0300

    Update

--- src/Components/FormularioContato/index.tsx
+++ src/Components/FormularioContato/index.tsx
@@ -1,28 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormContact, InputGroup, FormsContactGroup, Button, MensageAndButton, FormMainContact, TitleFormContact } from './styled';
-import { handleSubmitForms } from '../../services/send_email';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import API_URL from '../../services';
 
 const FormularioContato: React.FC = () => {
+    const [nome, setNome] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [mensagem, setMensagem] = useState<string>('');
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        let formErrors = false;
+
+        if (nome.trim() === '') {
+            formErrors = true;
+            toast.error('Nome não pode ficar vazio.');
+        }
+
+        if (!isEmail(email)) {
+            formErrors = true;
+            toast.error('Email inválido');
+        }
+        if (mensagem.trim() === '') {
+            formErrors = true;
+            toast.error('Mensagem não pode ficar vazio.');
+        }
+
+        if (formErrors) return;
+
+        // mandar o payload para o backend, Nome, email e Mensagem
+        try {
+            await API_URL.get('/email');
+            toast.success('Email enviado com sucesso!');
+
+            // await axios.post(`/contas/`, {
+            //     dono_conta,
+            //     login_conta,
+            //     senha_conta,
+            // });
+        } catch (error) {
+            console.log(error);
+            toast.error('Ocorreu um erro ao enviar o email. Tente novamente mais tarde.');
+        }
+    };
+
     return (
         <FormMainContact>
             <TitleFormContact>
                 <h1>Contato</h1>
                 <p>Entre em contato comigo utilizando o formulário abaixo ou através das minhas principais redes ao lado.</p>
             </TitleFormContact>
-            <FormContact onSubmit={handleSubmitForms}>
+
+            <FormContact onSubmit={handleSubmit}>
                 <InputGroup>
                     <FormsContactGroup>
-                        <input type="text" placeholder=" " />
+                        <input type="text" placeholder=" " value={nome} onChange={(e) => setNome(e.target.value)} />
                         <label>Nome</label>
                     </FormsContactGroup>
                     <FormsContactGroup>
-                        <input type="email" placeholder=" " />
+                        <input type="email" placeholder=" " value={email} onChange={(e) => setEmail(e.target.value)} />
                         <label>E-Mail</label>
                     </FormsContactGroup>
                 </InputGroup>
                 <MensageAndButton>
                     <FormsContactGroup>
-                        <textarea placeholder=" "></textarea>
+                        <textarea placeholder=" " value={mensagem} onChange={(e) => setMensagem(e.target.value)}></textarea>
                         <label>Digite sua mensagem...</label>
                     </FormsContactGroup>
                     <Button type="submit">Enviar</Button>
--- src/styles/GlobalStyled.tsx
+++ src/styles/GlobalStyled.tsx
@@ -3,7 +3,6 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 * {
     margin: 0;
     padding: 0;
@@ -43,12 +42,12 @@ body {
     left: 0;
     width: 100%;
     height: 100%;
-    z-index: 0; /* Para garantir que está no fundo */
+    z-index: 0;
 }
 
 .content-container {
     position: relative;
-    z-index: 1; /* Para garantir que está acima dos particles */
+    z-index: 1;
 }
 
 `;
