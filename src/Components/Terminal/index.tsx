b577b220f388abac1239992846660d3ff93ab4b8
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-27 18:12:12 -0300

    atualizacao do componente

--- src/Components/Terminal/index.tsx
+++ src/Components/Terminal/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import MaquinaWriteAbout from '../MaquinaWriteAbout';
-import { AbasPowerShell, PowerShell, PwsNewTabs, PwsTabs, PwsTabsButtons, PwsWindowButtons, PwsTabsTitle, ContentPSW, ContainerButtonProjectPSW, LinkStyled } from './styled';
+import { AbasPowerShell, PowerShell, PwsNewTabs, PwsTabs, PwsTabsButtons, PwsWindowButtons, PwsTabsTitle, ContentPSW, ContainerButtonProjectPSW, LinkStyled, InformacoesTecnicas } from './styled';
 import * as Icon from 'react-icons/vsc';
+import LanguagesComponent from '../LanguagesComponent';
+import { databasesObj, frameworksObj, hostingObj, languagesObj, sistemasEferramentasObj } from './data';
 
 const Terminal: React.FC = () => {
     const [showContent, setShowContent] = useState(false);
@@ -57,31 +59,107 @@ const Terminal: React.FC = () => {
                     <div>
                         <p>PortFolio 1.0.0</p>
                     </div>
-                    <div>
+                    <div className="comands">
                         <p>C:\Sobre\edsu&gt;</p>
                         <MaquinaWriteAbout text="cat sobre-mim.txt" />
                     </div>
 
+                    {/* <div className="status-github">
+                                <a href="https://github.com/anuraghazra/github-readme-stats">
+                                    <img
+                                        src="https://github-readme-stats.vercel.app/api/top-langs/?username=edsuuu&layout=donut-vertical&langs_count=10&theme=github_dark&border_color=00a4ef&bg_color=000000"
+                                        alt="github-readme-stats"
+                                    />
+                                </a>
+                            </div> */}
+
                     <div className={showContent ? 'show-content' : ''}>
                         <ContainerButtonProjectPSW className="container-button-project-psw">
-                            <div>
-                                <h1>Pequena descrição sobre mim</h1>
+                            <>
+                                <h2>Pequena descrição sobre mim</h2>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi illo, distinctio repellendus exercitationem est dicta eius ut impedit odit. Sunt culpa in totam
-                                    illum, nemo a aliquam sequi omnis possimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi illo, distinctio repellendus exercitationem est dicta
-                                    eius ut impedit odit. Sunt culpa in totam illum, nemo a aliquam sequi omnis possimus?
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quaerat odio vero mollitia unde quas magni deserunt magnam eos quisquam alias, officia reiciendis?
+                                    Hic non neque repellendus maxime, aliquid quaerat.
                                 </p>
-                            </div>
+                            </>
                             <br />
-
                             <h1>Soft skills</h1>
-                            <h1>Skills, linguagens, databases, ferramentas, frameworks</h1>
-                            <br />
-                            {/* <h3>Download para o meu currículo</h3> */}
+                            <InformacoesTecnicas>
+                                <div className="card-infos">
+                                    <h1>Linguagens</h1>
+                                    <div className="content-icons">
+                                        {languagesObj.map((item, index) => (
+                                            <LanguagesComponent key={index} name={item.name} Icon={item.Icon} />
+                                        ))}
+                                    </div>
+                                    <p>
+                                        Aprendizado Contínuo: Gosto de estar sempre atualizado e aprender novas tecnologias e habilidades. Por isso, sempre procuro fazer cursos online para expandir
+                                        meus conhecimentos.
+                                    </p>
+                                </div>
+                                <div className="card-infos">
+                                    <h1>Frameworks</h1>
+                                    <div className="content-icons">
+                                        {frameworksObj.map((item, index) => (
+                                            <LanguagesComponent key={index} name={item.name} Icon={item.Icon} />
+                                        ))}
+                                    </div>
+                                    <p>
+                                        Aprendizado Contínuo: Gosto de estar sempre atualizado e aprender novas tecnologias e habilidades. Por isso, sempre procuro fazer cursos online para expandir
+                                        meus conhecimentos.
+                                    </p>
+                                </div>
+
+                                <div className="card-infos">
+                                    <div>
+                                        <h1>DevOps</h1>
+                                    </div>
+                                    <div className="content-icons">
+                                        {hostingObj.slice(0, 5).map((item, index) => (
+                                            <LanguagesComponent key={index} name={item.name} Icon={item.Icon} />
+                                        ))}
+                                    </div>
+                                    <div>
+                                        <p>Possuo habilidades em hospedagem de aplicações, sejam Single Page, Sistema Web e Banco de dados, nos maiores serviços de cloud</p>
+                                    </div>
+                                </div>
+                            </InformacoesTecnicas>
+
+                            <InformacoesTecnicas>
+                                <div className="card-infos">
+                                    <div>
+                                        <h2>Banco de dados</h2>
+                                    </div>
+                                    <div className="content-icons">
+                                        {databasesObj.map((item, index) => (
+                                            <LanguagesComponent key={index} name={item.name} Icon={item.Icon} />
+                                        ))}
+                                    </div>
+                                    <div>
+                                        <p>Utilizo Windows/Linux como sistema operacional, que me forneceu habilidades com Linha de Comando, Bash e Docker.</p>
+                                    </div>
+                                </div>
+
+                                <div className="card-infos">
+                                    <div>
+                                        <h2>Sistemas e Ferramentas</h2>
+                                    </div>
+                                    <div className="content-icons">
+                                        {sistemasEferramentasObj.map((item, index) => (
+                                            <LanguagesComponent key={index} name={item.name} Icon={item.Icon} />
+                                        ))}
+                                    </div>
+                                    <div>
+                                        <p>Utilizo Windows/Linux como sistema operacional, que me forneceu habilidades com Linha de Comando, Bash e Docker.</p>
+                                    </div>
+                                </div>
+                            </InformacoesTecnicas>
+
+                            <h3>visualizar meu currículo</h3>
                             <br />
 
                             <div>
-                                <LinkStyled to="/projetos"> Ir para Projetos</LinkStyled>
+                                <LinkStyled to="/projetos"> Ver meus Projetos</LinkStyled>
                             </div>
                         </ContainerButtonProjectPSW>
                     </div>
