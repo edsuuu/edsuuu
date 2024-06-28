3b313004333ca8ed601c3096200a0cad4dc5cbf0
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-28 20:45:11 -0300

    Add Responsive Navbar

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LogoNavbar from '../LogoNavbar';
-import { Navbar, LinksLista, LinkStyled } from './styled';
+import { Navbar, LinksLista, LinkStyled, Container } from './styled';
 import { RotasNavigationProps } from '../../interfaces';
+import { CgMenu } from 'react-icons/cg';
+import { IoMdClose } from 'react-icons/io';
 
 const rotas: RotasNavigationProps[] = [
     { name: 'Home', to: '/' },
@@ -11,19 +13,67 @@ const rotas: RotasNavigationProps[] = [
 ];
 
 const Navigation: React.FC = () => {
+    const [responsiveBackVisible, setResponsiveBackVisible] = useState(false);
+
+    //  para fechar o menu responsivo
+    const closeResponsiveBack = () => {
+        setResponsiveBackVisible(false);
+    };
+
+    const toggleResponsiveBack = () => {
+        setResponsiveBackVisible((prevState) => !prevState);
+    };
+    // para fechar se a tela for maior de 768px
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768) {
+                closeResponsiveBack();
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleLinkClick = () => {
+        closeResponsiveBack();
+    };
+
     return (
-        <Navbar>
-            <LinksLista>
-                <LogoNavbar />
-                <div className="links">
-                    {rotas.map((rota, index) => (
-                        <div key={index}>
-                            <LinkStyled to={rota.to}>{rota.name}</LinkStyled>
-                        </div>
-                    ))}
+        <Container>
+            <Navbar>
+                <LinksLista>
+                    <LogoNavbar />
+                    <div className="links">
+                        {rotas.map((rota, index) => (
+                            <div key={index}>
+                                <LinkStyled to={rota.to} onClick={handleLinkClick}>
+                                    {rota.name}
+                                </LinkStyled>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="hamburguer" onClick={toggleResponsiveBack}>
+                        {responsiveBackVisible === false ? <CgMenu size={35} /> : <IoMdClose size={35} />}
+                    </div>
+                </LinksLista>
+            </Navbar>
+            {responsiveBackVisible && (
+                <div className="responsive-back">
+                    <div className="responsive-links">
+                        {rotas.map((rota, index) => (
+                            <div key={index} className="links-div">
+                                <LinkStyled to={rota.to} onClick={handleLinkClick}>
+                                    {rota.name}
+                                </LinkStyled>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </LinksLista>
-        </Navbar>
+            )}
+        </Container>
     );
 };
 
