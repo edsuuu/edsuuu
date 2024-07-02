e1192e636d1d44136c11c74ad8c7f4359fee0b87
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-07-02 13:51:01 -0300

    fix script

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -11,13 +11,7 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
     echo "Build realizado com sucesso."
 
-    sudo rm -rf /home/edson/portfolio/express-from-deploy-static-pages/dist/
-    echo "PASTA REMOVIDA DA FEATURE ANTERIOR"
-
-    sudo mv /home/edson/portfolio/edsuuu/dist /home/edson/portfolio/express-from-deploy-static-pages/
-    echo "PASTA MOVIDA PARA DEPLOY"
-
-    pm2 restart 0
+    pm2 restart 3
     echo "PM2 REINICIADO."
 
     echo "Deploy realizado com sucesso."
